30f9d66db39f8ee30e8507ae6405a57cea69343a
Guilherme Thales <guilhermevtog@hotmail.com>
2019-02-19 03:13:28 +0000

    start study with React

---
+++ src/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import React from 'react';
+
+const Title = () => {
+  return (
+    <div>
+      <h1>Título da Página</h1>
+    </div>
+  )
+}
+
+export default Title;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+'use strit';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Title from './app';
+
+ReactDOM.render(
+  <Title />,
+  document.querySelector('[data-js="app"]')
+)
