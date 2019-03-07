43d04976d2997a8503f1d8c05c8c8977b0df202f
Guilherme Thales <guilhermevtog@hotmail.com>
2019-03-07 03:32:32 +0000

    state and arrow functions

--- src/app.js
+++ src/app.js
@@ -1,15 +1,26 @@
 'use strict';
 
 import React, { Component } from 'react';
-import LikeButton from './like-button';
-import SearchButton from './search-button';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      text: 'Guilherme'
+    }
+  }
+
+  setName = () => {
+    this.setState({
+      text: 'Xobi'
+    });
+  }
+
   render() {
     return (
-      <div className="container">
-        <LikeButton />
-        <SearchButton />
+      <div className="container" onClick={ this.setName }>
+        { this.state.text }
       </div>
     )
   }
