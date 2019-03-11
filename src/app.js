564ef6bd535815c176252dab73a752d16bb742b8
Guilherme Thales <guilhermevtog@hotmail.com>
2019-03-11 02:38:37 +0000

    learning about propTypes to typecheck props for a component

--- src/app.js
+++ src/app.js
@@ -2,41 +2,12 @@
 
 import React, { Component } from 'react';
 import Button from './button';
-import Square from './square';
-import Timer from './timer';
 
 class App extends Component {
-
-  constructor(props) {
-    console.log('constructor app');
-    super(props)
-
-    this.state = {
-      time: 0,
-      showTimer: true
-    }
-  }
-
-  setColorSquare = (color) => {
-    this.setState({
-      color: color
-    })
-  }
-
-  componentWillMount() {
-    console.log('componentWillMount app');
-  }
-
-  componentDidMount() {
-    console.log('componentDidMount app');
-  }
-
   render() {
-    console.log('render app');
     return (
       <div>
-        <Timer time={ this.state.time } />
-        <button onClick={() => this.setState({ time: this.state.time + 10 })}>Change Props</button>
+        <Button handleClick={() => console.log('clicou')}>Clique em mim</Button>
       </div>
     )
   }
--- src/button.js
+++ src/button.js
@@ -8,4 +8,8 @@ const Button = ({ children, handleClick }) => {
   )
 };
 
+Button.propTypes = {
+  handleClick: React.PropTypes.func.isRequired
+}
+
 export default Button;
