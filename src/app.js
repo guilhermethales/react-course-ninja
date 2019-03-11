700d28e2a04884602e1cdb4a7fc892506f706046
Guilherme Thales <guilhermevtog@hotmail.com>
2019-03-11 03:10:33 +0000

    forms (textarea)

--- src/app.js
+++ src/app.js
@@ -7,22 +7,18 @@ class App extends Component {
     super(props);
 
     this.state = {
-      value: '3',
+      value: 'valor inicial',
     }
   }
   render() {
     return (
       <div>
         <form>
-          <select value={ this.state.value } onChange={ (e) => {
-              this.setState({
-                value: e.target.value
-              })
-            } }>
-            <option value="1">Opção 1</option>
-            <option value="2">Opção 2</option>
-            <option value="3">Opção 3</option>
-          </select>
+          <textarea value={ this.state.value } onChange={ (e) => {
+            this.setState({
+              value: e.target.value
+            })
+          } } cols="30" rows="10" />
         </form>
       </div>
     )
