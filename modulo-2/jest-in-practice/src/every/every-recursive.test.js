a80c70a544612ff457eb80c6d351d69618350075
= <=>
2019-04-11 00:17:00 -0300

    every method with tdd

---
+++ modulo-2/jest-in-practice/src/every/every-recursive.test.js
@@ -0,0 +1,28 @@
+'use strict';
+
+import { expect } from 'chai';
+import every from './every-recursive';
+
+it('every should be a function', () => {
+  expect(every).to.be.a('function');
+});
+
+it('every([], (item) => item) should return true', () => {
+	expect(every([], (item) => item)).to.be.true;
+})
+
+it('every([5, 10, 15, 20], (item) => item > 10) should return false', () => {
+	expect(every([5, 10, 15, 20], (item) => item > 10)).to.be.false;
+})
+
+it('every([15, 20], (item) => item > 10) should return true', () => {
+	expect(every([15, 20], (item) => item > 10)).to.be.true;
+})
+
+it('every([1, 2, 3], (item, index) => index === item - 1) should return true', () => {
+	expect(every([1, 2, 3], (item, index) => index === item - 1)).to.be.true;
+})
+
+it('every([1, 2, 3], (item, index, array) => array.length === 3) should return true', () => {
+	expect(every([1, 2, 3], (item, index, array) => array.length === 3)).to.be.true;
+})
