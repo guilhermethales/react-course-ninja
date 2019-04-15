fc4ac2a672d2577d22d2d8824a37302f3119500d
= <=>
2019-04-14 22:47:56 -0300

    Add Webpack Dashboard

--- modulo-2/working-with-assets/github-app/webpack.config.js
+++ modulo-2/working-with-assets/github-app/webpack.config.js
@@ -5,6 +5,7 @@ const webpack = require('webpack');
 const validate = require('webpack-validator');
 const HtmlPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const DashboardPlugin = require('webpack-dashboard/plugin');
 
 module.exports = validate({
   devtool: 'source-map',
@@ -22,6 +23,7 @@ module.exports = validate({
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new ExtractTextPlugin('[name]-[hash].css'),
+    new DashboardPlugin(),
     new HtmlPlugin({
       title: 'Github App',
       template: path.join(__dirname, 'src', 'html', 'template.html')
