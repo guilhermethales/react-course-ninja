00ece223272285a840baa42b2c7ce9813a606933
= <=>
2019-04-18 23:38:33 -0300

    Setting the storybook

---
+++ modulo-2/working-with-assets/github-app/src/components/actions/actions.story.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import { storiesOf } from '@kadira/storybook';
+import React from 'react';
+
+storiesOf('Div', module)
+  .add('first story', () => (
+    <div>Hello</div>
+  ))
---
+++ modulo-2/working-with-assets/github-app/src/components/repos/repos.story.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import { storiesOf } from '@kadira/storybook';
+import React from 'react';
+
+storiesOf('Repos', module)
+  .add('first repos story', () => (
+    <h1>Repos</h1>
+  ))
