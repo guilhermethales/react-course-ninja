b97f535fcf089de093cdc0c1fd929f9d3588ab63
= <=>
2019-05-01 02:53:34 -0300

    Pagination - Part 2 of implementation

--- modulo-2/working-with-assets/github-app/src/utils/pagination/index.js
+++ modulo-2/working-with-assets/github-app/src/utils/pagination/index.js
@@ -1,7 +1,26 @@
 'use strict';
 
-const pagination = ({total, activePage}) => {
-  return Array.from({ length: total }, (_, i) => i + 1);
+const centerRule = ({ total, activePage }) => {
+  if(activePage - 1 <= 0)
+    return 1;
+  return activePage - 1;
+}
+
+const pagination = ({total, activePage}) => { 
+  if(total <= 5) {
+    return Array.from({ length: total }, (_, i) => i + 1);
+  }
+
+  const visiblePages = 3;
+  let pages = [
+    1, 
+    ...Array.from({ length: visiblePages }, (_, i) => i + centerRule({ total, activePage })),
+    total
+  ];
+
+  pages = pages.filter((page, index, array) => array.indexOf(page) === index);
+
+  return pages;
 }
 
 export default pagination;
--- modulo-2/working-with-assets/github-app/src/utils/pagination/pagination.test.js
+++ modulo-2/working-with-assets/github-app/src/utils/pagination/pagination.test.js
@@ -5,17 +5,38 @@ test('pagination should be a function', () => {
   expect(pagination).to.be.a('function');
 })
 
-test('pagination({ total: 1, activePage: 1 }) should be a function', () => {
+test('pagination({ total: 1, activePage: 1 }) should return [1]', () => {
   const params = { total: 1, activePage: 1 }
-  expect(pagination(params)).to.be.deep.equal([1]);
+  const result = [1];
+  expect(pagination(params)).to.be.deep.equal(result);
 })
 
-test('pagination({ total: 2, activePage: 1 }) should be a function', () => {
+test('pagination({ total: 2, activePage: 1 }) should return [1, 2]', () => {
   const params = { total: 2, activePage: 1 }
-  expect(pagination(params)).to.be.deep.equal([1, 2]);
+  const result = [1, 2];
+  expect(pagination(params)).to.be.deep.equal(result);
 })
 
-test('pagination({ total: 4, activePage: 1 }) should be a function', () => {
-  const params = { total: 4, activePage: 1 }
-  expect(pagination(params)).to.be.deep.equal([1, 2, 3, 4]);
+test('pagination({ total: 5, activePage: 1 }) should return [1, 2, 3, 4, 5]', () => {
+  const params = { total: 5, activePage: 1 }
+  const result = [1, 2, 3, 4, 5];
+  expect(pagination(params)).to.be.deep.equal(result);
+})
+
+test('pagination({ total: 6, activePage: 1 }) should return [1, 2, 3, ..., 6]', () => {
+  const params = { total: 6, activePage: 1 }
+  const result = [1, 2, 3, '...', 6];
+  expect(pagination(params)).to.be.deep.equal(result);
+})
+
+test('pagination({ total: 6, activePage: 2 }) should return [1, 2, 3, ..., 6]', () => {
+  const params = { total: 6, activePage: 2 }
+  const result = [1, 2, 3, '...', 6];
+  expect(pagination(params)).to.be.deep.equal(result);
+})
+
+test('pagination({ total: 6, activePage: 3 }) should return [1, 2, 3, 4, 5, 6]', () => {
+  const params = { total: 6, activePage: 3 }
+  const result = [1, 2, 3, 4, 5, 6];
+  expect(pagination(params)).to.be.deep.equal(result);
 })
