b702c04382f952236a9d0fc68660e4f89b38724e
guilhermethalesmereo <guilherme.simoes@mereo.com>
2019-06-25 20:01:57 -0300

    Pagination Storybook - Part 3

--- modulo-2/working-with-assets/github-app/src/components/pagination/index.js
+++ modulo-2/working-with-assets/github-app/src/components/pagination/index.js
@@ -1,19 +1,30 @@
 'use scrict'
 
-import React from 'react'
+import React, {PropTypes} from 'react'
 import pagination from 'utils/pagination/index'
 import Page from './page'
 
-const Pagination = ({total, activePage, pageLink}) => (
+const Pagination = ({total, activePage, pageLink, onClick}) => (
   <div>
     <ul>
       {pagination({total, activePage}).map((page, i) => (
         <li key={i}>
-          <Page page={page} pageLink={pageLink.replace('%page%', page)} />
+          <Page page={page} pageLink={pageLink.replace('%page%', page)} onClick={onClick} />
         </li>
       ))}
     </ul>
   </div>
 )
+
+Pagination.defaultProps = {
+  pageLink: ''
+}
+
+Pagination.propTypes = {
+  total: PropTypes.number,
+  activePage: PropTypes.number,
+  pageLink: PropTypes.string,
+  onClick: PropTypes.func
+}
 
 export default Pagination
--- modulo-2/working-with-assets/github-app/src/components/pagination/pagination.story.js
+++ modulo-2/working-with-assets/github-app/src/components/pagination/pagination.story.js
@@ -7,5 +7,23 @@ import Pagination from './index'
 const stories = storiesOf('<Pagination />', module)
 
 stories.add('without props', () => (
-  <Pagination total={10} pageLink='http://mypage.com/page/%page%' />
+  <Pagination />
+))
+
+stories.add('with total and activePage', () => (
+  <Pagination total={10} activePage={5} />
+))
+
+stories.add('with total and activePage', () => (
+  <Pagination total={10} activePage={5} />
+))
+
+stories.add('with total, activePage and pageLink', () => (
+  <Pagination total={3} activePage={1} pageLink='http://mypage.com/page/%page%' />
+))
+
+stories.add('with total, activePage, pageLink and callback', () => (
+  <Pagination total={15} activePage={7} pageLink='http://mypage.com/page/%page%' onClick={(page) => {
+    window.alert(page)
+  }} />
 ))
