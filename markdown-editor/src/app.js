ea53533aef864a221109a3492e73e50a54d408a1
guilhermethales <guilhermevtog@hotmail.com>
2019-07-23 20:10:12 -0300

    start markdown editor application

---
+++ markdown-editor/src/app.js
@@ -0,0 +1,34 @@
+'use strict'
+
+import React, { Component } from 'react'
+
+import './css/style.css'
+
+class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      value: ''
+    }
+
+    this.handleSubmit = (e) => {
+      e.preventDefault()
+      this.setState({
+        value: e.target.textarea.value
+      })
+    }
+  }
+  render () {
+    return (
+      <div className='editor'>
+        <form onSubmit={this.handleSubmit}>
+          <textarea name='textarea' />
+          <button type='submit'>Renderizar Markup</button>
+        </form>
+        <div className='view'>{this.state.value}</div>
+      </div>
+    )
+  }
+}
+
+export default App
